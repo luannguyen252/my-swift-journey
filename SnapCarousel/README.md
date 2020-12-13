d12d4766afaadb34cebb04941a5df04078731402
Luan Nguyen <design.luannguyen@gmail.com>
2020-12-13 16:56:25 +0700

    Snap Carousel

---
+++ SnapCarousel/README.md
@@ -0,0 +1,11 @@
+# Snap Carousel
+
+> Build a Snap Carousel with SwiftUI.
+
+![Snap Carousel 1](./SnapCarousel_1.png "Snap Carousel 1")
+
+![Snap Carousel 2](./SnapCarousel_2.png "Snap Carousel 2")
+
+![Snap Carousel 3](./SnapCarousel_3.png "Snap Carousel 3")
+
+---
---
+++ SnapCarousel/SnapCarousel/SnapCarouselApp.swift
@@ -0,0 +1,18 @@
+//
+//  SnapCarouselApp.swift
+//  SnapCarousel
+//
+//  Created by Luan Nguyen on 13/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct SnapCarouselApp: App {
+    // MARK: - BODY
+    var body: some Scene {
+        WindowGroup {
+            ContentView(UIState: .init())
+        }
+    }
+}
