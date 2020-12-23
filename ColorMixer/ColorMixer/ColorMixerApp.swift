9df44575ae5b45ac30d4800ba796fdfcea62582d
Luan Nguyen <design.luannguyen@gmail.com>
2020-12-23 17:01:19 +0700

    Color Mixer

---
+++ ColorMixer/ColorMixer/ColorMixerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorMixerApp.swift
+//  ColorMixer
+//
+//  Created by Luan Nguyen on 23/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct ColorMixerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView(colorR: 0.5, colorG: 0.5, colorB: 0.5)
+        }
+    }
+}
---
+++ ColorMixer/README.md
@@ -0,0 +1,11 @@
+# Color Mixer
+
+> Build Color Mixer application with SwiftUI.
+
+![Color Mixer 1](./ColorMixer_1.png "Color Mixer 1")
+
+![Color Mixer 2](./ColorMixer_2.png "Color Mixer 2")
+
+![Color Mixer 3](./ColorMixer_3.png "Color Mixer 3")
+
+---
