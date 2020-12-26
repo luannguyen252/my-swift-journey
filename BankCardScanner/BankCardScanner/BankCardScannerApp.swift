ceb7e1bc6a46783591e23e7375ae7801227cfe0c
Luan Nguyen <design.luannguyen@gmail.com>
2020-12-26 16:54:15 +0700

    Bank Card Scanner

---
+++ BankCardScanner/BankCardScanner/BankCardScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BankCardScannerApp.swift
+//  BankCardScanner
+//
+//  Created by Luan Nguyen on 26/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct BankCardScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
