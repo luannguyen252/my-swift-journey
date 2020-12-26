7ff867cad78ceab110287585d1e2c3f78c09ef59
Luan Nguyen <design.luannguyen@gmail.com>
2020-12-26 17:54:48 +0700

    Initial commit

---
+++ SwiftUIAirConditionerController/SwiftUIAirConditionerController/SwiftUIAirConditionerControllerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAirConditionerControllerApp.swift
+//  SwiftUIAirConditionerController
+//
+//  Created by Luan Nguyen on 26/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAirConditionerControllerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
