542f043df121ada9c210441d123a5fed57f87eb1
Luan Nguyen <design.luannguyen@gmail.com>
2021-01-06 14:14:21 +0700

    SwiftUI Project 17 Concepts

---
+++ SwiftUI-Project17Concepts/Project17Concepts/Project17Concepts/Reusables/Reusables.swift
@@ -0,0 +1,21 @@
+//
+//  Reusables.swift
+//  Project17Concepts
+//
+//  Created by Luan Nguyen on 06/01/2021.
+//  Copyright © 2021 CypherPoet. All rights reserved.
+//
+
+import SwiftUI
+
+struct Reusables: View {
+    var body: some View {
+        Text("Reusables.")
+    }
+}
+
+struct Reusables_Previews: PreviewProvider {
+    static var previews: some View {
+        Reusables()
+    }
+}
---
+++ SwiftUI-Project17Concepts/README.md
@@ -0,0 +1,1 @@
+# SwiftUI Project 17 Concepts
