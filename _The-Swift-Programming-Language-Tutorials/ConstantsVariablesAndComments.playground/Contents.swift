860b991cbd3db7e90f715042861c32a572b18255
Luan Nguyen <design.luannguyen@gmail.com>
2021-01-06 09:49:15 +0700

    The Swift Programming Language Tutorials

---
+++ _The-Swift-Programming-Language-Tutorials/ConstantsVariablesAndComments.playground/Contents.swift
@@ -0,0 +1,45 @@
+//
+//  Apple Swift version 5.2.4
+//  Xcode Version 11.5 beta 2 (11N605f)
+//
+//  Created by Petro Onishchuk on Jun/10/20.
+//  Copyright © 2020 Petro Onishchuk. All rights reserved.
+//
+//  The Swift Programming Language.
+//  Topic: Constants, Variables and Comments.
+//
+
+import UIKit
+
+// 1.Comments
+
+// Single line comment
+
+/*
+ First line
+ Second line
+     /*
+     First nested line
+     Second nested line
+     */
+ */
+
+// 2.Constants and Variables
+
+let numberOfMonths = 12
+//numberOfMonths = 10
+
+var nameOfMonth = "January"
+nameOfMonth = "May"
+nameOfMonth = "April"
+
+// 3.Type Annotations
+
+let randomNumber: Int
+randomNumber = 10
+
+var movieTitle: String
+movieTitle = "Avengers"
+
+var isConnected = true
+isConnected = false
