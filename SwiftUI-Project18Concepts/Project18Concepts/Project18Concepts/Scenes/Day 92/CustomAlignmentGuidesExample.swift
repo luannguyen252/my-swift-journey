fddbd46518f87194bbec09fea7198727920da3aa
Luan Nguyen <design.luannguyen@gmail.com>
2021-01-06 14:02:21 +0700

    SwiftUI Project 18 Concepts

---
+++ SwiftUI-Project18Concepts/Project18Concepts/Project18Concepts/Scenes/Day 92/CustomAlignmentGuidesExample.swift
@@ -0,0 +1,74 @@
+//
+//  CustomAlignmentGuidesExample.swift
+//  Project18Concepts
+//
+//  Created by CypherPoet on 1/23/20.
+// ✌️
+//
+
+import SwiftUI
+
+extension VerticalAlignment {
+    enum CustomAlignment: AlignmentID {
+        static func defaultValue(in dimensions: ViewDimensions) -> CGFloat {
+            dimensions[VerticalAlignment.center]
+        }
+        
+    }
+
+    static let customAlignment = VerticalAlignment(CustomAlignment.self)
+}
+
+struct CustomAlignmentGuidesExample {
+}
+
+// MARK: - View
+extension CustomAlignmentGuidesExample: View {
+    var body: some View {
+        HStack(alignment: .customAlignment) {
+            VStack {
+                Text("Twitter: @Cypher_Poet")
+                    .alignmentGuide(.customAlignment) { dimensions in
+                        dimensions[VerticalAlignment.top]
+                    }
+                
+                Image("CypherPoet")
+                    .resizable()
+                    .frame(width: 64, height: 64)
+            }
+            
+            Spacer()
+            
+            VStack {
+                Text("Species")
+                
+                Text("HUMAN")
+                    .font(.largeTitle)
+                    .alignmentGuide(.customAlignment) { dimensions in
+                        dimensions[VerticalAlignment.bottom]
+                    }
+                
+            }
+        }
+        .padding(.horizontal)
+    }
+}
+
+// MARK: - Computeds
+extension CustomAlignmentGuidesExample {
+}
+
+// MARK: - View Variables
+extension CustomAlignmentGuidesExample {
+}
+
+// MARK: - Private Helpers
+private extension CustomAlignmentGuidesExample {
+}
+
+// MARK: - Preview
+struct CustomAlignmentGuidesExample_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomAlignmentGuidesExample()
+    }
+}
---
+++ SwiftUI-Project18Concepts/Project18Concepts/Project18Concepts/Scenes/RootView.swift
@@ -0,0 +1,70 @@
+//
+//  RootView.swift
+//  Project18Concepts
+//
+//  Created by CypherPoet on 1/23/20.
+// ✌️
+//
+
+import SwiftUI
+
+struct RootView {  }
+
+// MARK: - View
+extension RootView: View {
+    var body: some View {
+        NavigationView {
+            List {
+                Section(header: Text("First Examples").font(.headline)) {
+                    NavigationLink(
+                        "Alignment & Alignment Guides",
+                        destination: AlignmentAndGuidesExample()
+                    )
+                    NavigationLink(
+                        "Custom Alignment Guides",
+                        destination: CustomAlignmentGuidesExample()
+                    )
+                }
+                
+                Section(header: Text("Second Examples").font(.headline)) {
+                    NavigationLink(
+                        "Absolute Positioning",
+                        destination: AbsolutePositioningExample()
+                    )
+                    NavigationLink(
+                        "GeometryReader & Coordinate Spaces",
+                        destination: GeometryReaderAndCoordinatesExample()
+                    )
+                    NavigationLink(
+                        "Scroll View Effects with GeometryReaders (1)",
+                        destination: NestedGeometryReadersHelixExample()
+                    )
+                    NavigationLink(
+                        "Scroll View Effects with GeometryReaders (2)",
+                        destination: NestedGeometryReadersCoverFlowExample()
+                    )
+                }
+            }
+            .navigationBarTitle("Geometry & Layout")
+        }
+    }
+}
+
+// MARK: - Computeds
+extension RootView {
+}
+
+// MARK: - View Variables
+extension RootView {
+}
+
+// MARK: - Private Helpers
+private extension RootView {
+}
+
+// MARK: - Preview
+struct RootView_Previews: PreviewProvider {
+    static var previews: some View {
+        RootView()
+    }
+}
