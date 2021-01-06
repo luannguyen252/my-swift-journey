20b6b350f675e96f52183510e1be6ebcc99b372f
Luan Nguyen <design.luannguyen@gmail.com>
2021-01-06 13:13:19 +0700

    SwiftUI WordScramble

---
+++ SwiftUI-WordScramble/WordScramble/WordScramble/Data/Data.swift
@@ -0,0 +1,21 @@
+//
+//  Data.swift
+//  WordScramble
+//
+//  Created by Luan Nguyen on 06/01/2021.
+//  Copyright © 2021 CypherPoet. All rights reserved.
+//
+
+import SwiftUI
+
+struct Data: View {
+    var body: some View {
+        Text("Data.")
+    }
+}
+
+struct Data_Previews: PreviewProvider {
+    static var previews: some View {
+        Data()
+    }
+}
