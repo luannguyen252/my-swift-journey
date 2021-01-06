d6d935cf8f34620c615c5121250e66fc418d8aec
Luan Nguyen <design.luannguyen@gmail.com>
2021-01-06 11:35:23 +0700

    SwiftUI Bookworm CoreData

---
+++ SwiftUI-Bookworm-CoreData/Bookworm/Bookworm/Data/Models/Book/Book+CoreDataClass.swift
@@ -0,0 +1,13 @@
+//
+//  Book+CoreDataClass.swift
+//  Bookworm
+//
+//  Created by CypherPoet on 11/16/19.
+// ✌️
+//
+
+import Foundation
+import CoreData
+
+@objc(Book)
+public class Book: NSManagedObject {  }
