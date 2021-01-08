5cab4a7341c56136df04dc0cd8c3af2e6b4ec7cb
Luan Nguyen <design.luannguyen@gmail.com>
2021-01-08 08:27:23 +0700

    SwiftUI Unsplash API

---
+++ SwiftUIUnsplashAPI/SwiftUIUnsplashAPI/SwiftUIUnsplashAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIUnsplashAPIApp.swift
+//  SwiftUIUnsplashAPI
+//
+//  Created by Luan Nguyen on 08/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIUnsplashAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
