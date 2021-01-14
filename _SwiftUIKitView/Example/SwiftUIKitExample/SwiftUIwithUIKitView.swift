0039164e0174d94fb4debd1fcc77d55214ccff29
Luan Nguyen <design.luannguyen@gmail.com>
2021-01-14 11:12:19 +0700

    SwiftUIKit View

---
+++ _SwiftUIKitView/Example/SwiftUIKitExample/SwiftUIwithUIKitView.swift
@@ -0,0 +1,41 @@
+//
+//  SwiftUIwithUIKitView.swift
+//  SwiftUIKitExample
+//
+//  Created by Antoine van der Lee on 28/12/2020.
+//
+
+import SwiftUI
+import SwiftUIKitView
+
+struct SwiftUIwithUIKitView: View {
+    var body: some View {
+        NavigationView {
+            UIKitView() // <- This is a `UIKit` view.
+                .swiftUIView(layout: .intrinsic) // <- This is a SwiftUI `View`.
+                .set(\.title, to: "Hello, UIKit!")
+                .set(\.backgroundColor, to: UIColor(named: "swiftlee_orange"))
+                .fixedSize()
+                .navigationTitle("Use UIKit in SwiftUI")
+        }
+    }
+}
+
+struct SwiftUIwithUIKitView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIwithUIKitView()
+    }
+}
+
+// MARK: - UILabel Preview Example
+struct UILabelExample_Preview: PreviewProvider {
+    static var previews: some View {
+        UILabel() // <- This is a `UIKit` view.
+            .swiftUIView(layout: .intrinsic) // <- This is a SwiftUI `View`.
+            .set(\.text, to: "Hello, UIKit!") // <- Use key paths for updates.
+            .fixedSize() // <- Make sure the size is set
+            .previewLayout(.sizeThatFits)
+            .previewDisplayName("UILabel Preview Example")
+    }
+}
+
