6aeecca59a8caf579cb404e5cfa4ea3007595c3b
Luan Nguyen <design.luannguyen@gmail.com>
2021-01-14 17:23:08 +0700

    Alignment Guides Implicit

---
+++ SwiftUI-Alignment-Guides-Implicit/SwiftUI-Alignment-Guides-Implicit/SwiftUI_Alignment_Guides_ImplicitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Alignment_Guides_ImplicitApp.swift
+//  SwiftUI-Alignment-Guides-Implicit
+//
+//  Created by Luan Nguyen on 14/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Alignment_Guides_ImplicitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
