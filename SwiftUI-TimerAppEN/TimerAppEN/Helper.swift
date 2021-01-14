2c9450de754545a3c1d486c1591330749e32b13b
Luan Nguyen <design.luannguyen@gmail.com>
2021-01-14 14:55:33 +0700

    SwiftUI TimerAppEN

---
+++ SwiftUI-TimerAppEN/TimerAppEN/Helper.swift
@@ -0,0 +1,24 @@
+//
+//  Helper.swift
+//  TimerAppEN
+//
+//  Created by Farukh IQBAL on 07/04/2020.
+//  Copyright © 2020 Farukh IQBAL. All rights reserved.
+//
+
+import Foundation
+
+enum TimerMode {
+    case running
+    case paused
+    case initial
+}
+
+func secondsToMinutesAndSeconds(seconds: Int) -> String {
+    let minutes = "\((seconds % 3600) / 60)"
+    let seconds = "\((seconds % 3600) % 60)"
+    let minuteStamp = minutes.count > 1 ? minutes : "0" + minutes
+    let secondStamp = seconds.count > 1 ? seconds : "0" + seconds
+    
+    return "\(minuteStamp) : \(secondStamp)"
+}
