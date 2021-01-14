6cb2b8dc860e34989832d0411e07ec4a547ecd3e
Luan Nguyen <design.luannguyen@gmail.com>
2021-01-14 17:24:52 +0700

    SwiftUI Alignment Guides Tool

---
+++ SwiftUI-Alignment-Guides-Tool/SwiftUI-Alignment-Guides-Tool/SwiftUI_Alignment_Guides_ToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Alignment_Guides_ToolApp.swift
+//  SwiftUI-Alignment-Guides-Tool
+//
+//  Created by Luan Nguyen on 14/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Alignment_Guides_ToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
