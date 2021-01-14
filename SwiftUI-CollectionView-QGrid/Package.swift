353c1fdf503abdb538985b914523c31afd9e80ab
Luan Nguyen <design.luannguyen@gmail.com>
2021-01-14 21:33:43 +0700

    SwiftUI-CollectionView-QGrid

---
+++ SwiftUI-CollectionView-QGrid/Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:5.1
+
+import PackageDescription
+
+let package = Package(
+    name: "QGrid",
+    platforms: [
+      .iOS(.v13),
+      .macOS(.v10_15),
+      .tvOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "QGrid",
+            targets: ["QGrid"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "QGrid",
+            dependencies: []),
+    ]
+)
