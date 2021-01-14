3fe9c633bd2cf8ded624d9d94bb14d32e4bfae05
Luan Nguyen <design.luannguyen@gmail.com>
2021-01-14 17:06:31 +0700

    SwiftUI Masterclass Course iOS Development With Swift

---
+++ SwiftUI-Advanced-Animations/SwiftUI-Advanced-Animations/SwiftUI_Advanced_AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Advanced_AnimationsApp.swift
+//  SwiftUI-Advanced-Animations
+//
+//  Created by Luan Nguyen on 14/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Advanced_AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
