74aea0cd4559afe31bfe1204bc10a79782dd65c8
Luan Nguyen <design.luannguyen@gmail.com>
2021-01-14 17:30:38 +0700

    SwiftUI-AppleMusicGradient

---
+++ SwiftUI-AppleMusicGradient/AppleMusicGradient/SceneDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  SceneDelegate.swift
+//  AppleMusicGradient
+//
+//  Created by Swapnanil Dhol on 7/18/20.
+//  Copyright © 2020 Swapnanil Dhol. All rights reserved.
+//
+
+import UIKit
+import SwiftUI
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        let contentView = ContentView()
+        if let windowScene = scene as? UIWindowScene {
+            let window = UIWindow(windowScene: windowScene)
+            window.rootViewController = UIHostingController(rootView: contentView)
+            self.window = window
+            window.makeKeyAndVisible()
+        }
+    }
+}
+
