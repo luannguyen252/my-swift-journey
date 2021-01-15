d1683acad8ec2f645f185649fdb5698c95066819
Luan Nguyen <design.luannguyen@gmail.com>
2021-01-15 13:58:05 +0700

    SwiftUI-DataFlow

---
+++ _SwiftUI-DataFlow/DataFlow/Environment/GrandChildView.swift
@@ -0,0 +1,53 @@
+//
+//  GrandChildView.swift
+//  DataFlow
+//
+//  Created by Sarah Reichelt on 15/09/2019.
+//  Copyright © 2019 TrozWare. All rights reserved.
+//
+
+import SwiftUI
+
+struct GrandChildView: View {
+    // This grandchild view now gets access to the environment object
+    // even though its parent does not.
+
+    @EnvironmentObject var userSettings: UserSettings
+
+    var body: some View {
+        let imageName = userSettings.isLoggedIn
+            ? "person.crop.square"
+            : "questionmark.square"
+
+        let buttonText = userSettings.isLoggedIn
+            ? "Log Out"
+            : "Log In"
+
+        // because there is more than one statement in the body method
+        // the return keyword is needed to return the View
+        
+        return ZStack {
+            Color.blue
+
+            VStack {
+                Image(systemName: imageName).padding()
+
+                // Toggling the environment object value changes
+                // all the views that use it
+                Button(action: { self.userSettings.isLoggedIn.toggle() }) {
+                    Text(buttonText).modifier(GrayButtonStyle())
+                }
+            }
+            .padding()
+        }
+        .foregroundColor(.white)
+        .font(.system(size: 100))
+    }
+}
+
+struct GrandChildView_Previews: PreviewProvider {
+    static var previews: some View {
+        GrandChildView()
+            .environmentObject(UserSettings())
+    }
+}
---
+++ _SwiftUI-DataFlow/DataFlow/Observable 2/PersonDetailView.swift
@@ -0,0 +1,71 @@
+//
+//  PersonDetailView.swift
+//  DataFlow
+//
+//  Created by Sarah Reichelt on 14/09/2019.
+//  Copyright © 2019 TrozWare. All rights reserved.
+//
+
+import SwiftUI
+
+struct PersonDetailView: View {
+    // Data passed from parent list view
+    @Binding var person: PersonViewModel
+
+    // SwiftUI form with data fields
+    // note the autocapitalization and keyboard modifiers
+
+    var body: some View {
+        VStack {
+            Form {
+                Section(header: Text("First Name")) {
+                    TextField("Enter first name", text: $person.first)
+                        .autocapitalization(.words)
+                }
+
+                Section(header: Text("Last Name")) {
+                    TextField("Enter last name", text: $person.last)
+                        .autocapitalization(.words)
+                }
+
+                Section(header: Text("Phone Number")) {
+                    TextField("Enter phone number", text: $person.phone)
+                        .keyboardType(.phonePad)
+                }
+
+                Section(header: Text("Address")) {
+                    TextField("Address", text: $person.address)
+                        .autocapitalization(.words)
+                    TextField("City", text: $person.city)
+                        .autocapitalization(.words)
+                    TextField("State", text: $person.state)
+                        .autocapitalization(.words)
+                    TextField("Zip", text: $person.zip)
+                }
+            }
+
+            Text("Registered on:")
+                .font(.headline)
+                .padding(6)
+            Text("\(person.registered, formatter: dateFormatter)")
+        }
+    }
+
+    // Formatter for registration date
+    var dateFormatter: DateFormatter {
+        let df = DateFormatter()
+        df.timeStyle = .short
+        df.dateStyle = .long
+        return df
+    }
+}
+
+// Previewing requires using .constant to convert the data to a binding
+// Sample data is generated in an extension in Preview Content
+
+struct PersonDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        let person = PersonViewModel.samplePerson()
+        return PersonDetailView(person: .constant(person))
+    }
+}
