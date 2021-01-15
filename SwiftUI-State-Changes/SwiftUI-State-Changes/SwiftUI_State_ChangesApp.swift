f9ea9809f7f9b0ca710097be8a826c3e69d8ee9f
Luan Nguyen <design.luannguyen@gmail.com>
2021-01-15 08:53:08 +0700

    SwiftUI-State-Changes

---
+++ SwiftUI-State-Changes/SwiftUI-State-Changes/SwiftUI_State_ChangesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_State_ChangesApp.swift
+//  SwiftUI-State-Changes
+//
+//  Created by Luan Nguyen on 15/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_State_ChangesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
