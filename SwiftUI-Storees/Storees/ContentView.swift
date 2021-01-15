bbbc76fa0303c35f46543d336a1e5372c2d3eb5c
Luan Nguyen <design.luannguyen@gmail.com>
2021-01-15 08:27:50 +0700

    SwiftUI Storees

---
+++ SwiftUI-Storees/Storees/ContentView.swift
@@ -0,0 +1,75 @@
+import SwiftUI
+
+struct ContentView: View {
+    @EnvironmentObject var appData: AppData
+    @Environment(\.colorScheme) var colorScheme
+    @State var presentingStory = false
+    @State var selectedStory: Int?
+    @Namespace private var animation
+    
+    var body: some View {
+        ZStack {
+            VStack {
+                HStack {
+                    logo.scaledToFit()
+                    Spacer()
+                    Button(action: {
+                    }) {
+                        Image(systemName: "info.circle")
+                            .font(.system(size: 22, weight: .medium))
+                    }
+                }
+                .frame(height: 30)
+                .padding(.horizontal, 16)
+                .padding(.top, 12)
+                .padding(.bottom, 4)
+                
+                ScrollView {
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack (spacing: 0) {
+                            ForEach(appData.stories.indices) { index in
+                                let story = appData.stories[index]
+                                BubbleView(image: story.userImage,
+                                           username: story.username,
+                                           seen: story.lastUnseen == nil,
+                                           animation: animation,
+                                           onTap: {
+                                            withAnimation(.spring(response: 0.3, dampingFraction: 1)) {
+                                                presentingStory = true
+                                                selectedStory = index
+                                            }
+                                           }
+                                )
+                                .frame(width: 100, height: 100)
+                            }
+                        }
+                        .padding(.leading, 6)
+                    }
+                }
+            }
+            
+            if presentingStory && selectedStory != nil {
+                StoryView(selectedStory: Binding($selectedStory)!, isPresented: $presentingStory, animation: animation)
+            }
+        }
+        .accentColor(.primary)
+    }
+    
+    @ViewBuilder
+    var logo: some View {
+        if colorScheme == .dark {
+            Image("Logo")
+                .resizable()
+                .colorInvert()
+        } else {
+            Image("Logo")
+                .resizable()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView().environmentObject(AppData())
+    }
+}
