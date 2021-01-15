51e9ce66357375e7d0addee922ea0c39da36b6c6
Luan Nguyen <design.luannguyen@gmail.com>
2021-01-15 15:19:47 +0700

    SwiftUI-Dummy-Data-Demo

---
+++ SwiftUI-Dummy-Data-Demo/DummyDataDemo/DummyDataDemoApp.swift
@@ -0,0 +1,15 @@
+
+
+import SwiftUI
+
+
+@main
+struct DummyDataDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .colorScheme(.dark)
+                .accentColor(.green)
+        }
+    }
+}
