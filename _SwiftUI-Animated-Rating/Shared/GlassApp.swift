2f9ad1c7b50658e7346eae2e1da24141e260fc4a
Luan Nguyen <design.luannguyen@gmail.com>
2021-01-15 17:31:24 +0700

    SwiftUI-Animated-Rating

---
+++ _SwiftUI-Animated-Rating/Shared/GlassApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlassApp.swift
+//  Shared
+//
+//  Created by Balaji on 19/12/20.
+//
+
+import SwiftUI
+
+@main
+struct GlassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
