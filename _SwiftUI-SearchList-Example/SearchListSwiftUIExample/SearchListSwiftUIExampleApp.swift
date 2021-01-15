1ccf749f545bf9cbdbca7724fde07a63b991ed60
Luan Nguyen <design.luannguyen@gmail.com>
2021-01-15 09:48:40 +0700

    SwiftUI-SearchList-Example

---
+++ _SwiftUI-SearchList-Example/SearchListSwiftUIExample/SearchListSwiftUIExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchListSwiftUIExampleApp.swift
+//  SearchListSwiftUIExample
+//
+//  Created by John Codeos on 11/18/20.
+//
+
+import SwiftUI
+
+@main
+struct SearchListSwiftUIExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
