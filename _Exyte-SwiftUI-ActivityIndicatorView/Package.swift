ed874654e2243139b94e2acf2083e62573691e73
Luan Nguyen <design.luannguyen@gmail.com>
2021-01-19 09:00:07 +0700

    Exyte-SwiftUI-ActivityIndicatorView

---
+++ _Exyte-SwiftUI-ActivityIndicatorView/Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version:5.1
+
+import PackageDescription
+
+let package = Package(
+	name: "ActivityIndicatorView",
+	platforms: [
+		.macOS(.v10_15),
+        .iOS(.v13),
+        .watchOS(.v6),
+        .tvOS(.v13)
+    ],
+    products: [
+    	.library(
+    		name: "ActivityIndicatorView", 
+    		targets: ["ActivityIndicatorView"]
+    	)
+    ],
+    targets: [
+    	.target(
+    		name: "ActivityIndicatorView",
+            path: "Source"
+        )
+    ],
+    swiftLanguageVersions: [.v5]
+)
