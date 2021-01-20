5b21d0b5da6992db08378f06aafd148e736bdfae
Luan Nguyen <design.luannguyen@gmail.com>
2021-01-20 15:12:45 +0700

    TheAppWizard-SwiftUI-CardApp

---
+++ _TheAppWizard-SwiftUI-CardApp/CardAppSwiftUI/CardAppSwiftUI/BarView.swift
@@ -0,0 +1,40 @@
+import SwiftUI
+
+struct BarView: View {
+    var value: CGFloat = 0
+    var week: String = ""
+    
+    var body: some View {
+        VStack {
+            ZStack(alignment: .bottom) {
+                Capsule().frame(width: 20, height: value)
+                    .overlay( Capsule().stroke(Color.white, lineWidth: 2))
+                    .foregroundColor(.clear)
+                
+                Capsule().frame(width: 20, height: value)
+                    .overlay( Capsule().stroke(Color.white, lineWidth: 2))
+                    .foregroundColor(.clear)
+                
+                Capsule().frame(width: 20, height: value)
+                    .overlay( Capsule().stroke(Color.white, lineWidth: 2))
+                    .foregroundColor(.clear)
+                
+                Capsule().frame(width: 20, height: value)
+                    .overlay( Capsule().stroke(Color.white, lineWidth: 2))
+                    .foregroundColor(.clear)
+                
+                Capsule().frame(width: 20, height: value)
+                    .overlay( Capsule().stroke(Color.white, lineWidth: 2))
+                    .foregroundColor(.clear)
+            }
+            .padding()
+            Text(week)
+        }
+    }
+}
+
+struct BarView_Previews: PreviewProvider {
+    static var previews: some View {
+        BarView()
+    }
+}
