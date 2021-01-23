f9bb2ecb074c16fbbdf40cb8573dc071c680ddab
Luan Nguyen <design.luannguyen@gmail.com>
2021-01-23 16:21:40 +0700

    SwiftUI-PickerViewAsUITextFieldEntry

---
+++ SwiftUI-PickerViewAsUITextFieldEntry/PickerTextFieldExample/ViewController.swift
@@ -0,0 +1,37 @@
+import UIKit
+
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    @IBOutlet weak var theTextfield: UITextField!
+    
+    let myPickerData = [String](arrayLiteral: "Peter", "Jane", "Paul", "Mary", "Kevin", "Lucy")
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let thePicker = UIPickerView()
+        thePicker.delegate = self
+        theTextfield.inputView = thePicker
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: UIPickerView Delegation
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return myPickerData.count
+    }
+
+    func pickerView( _ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return myPickerData[row]
+    }
+
+    func pickerView( _ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        theTextfield.text = myPickerData[row]
+    }
+}
