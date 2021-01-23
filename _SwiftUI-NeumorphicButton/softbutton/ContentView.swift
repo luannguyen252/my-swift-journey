b9ce2da2990f551341688b43143db84daf3e8a52
Luan Nguyen <design.luannguyen@gmail.com>
2021-01-23 10:59:38 +0700

    SwiftUI-NeumorphicButton

---
+++ _SwiftUI-NeumorphicButton/softbutton/ContentView.swift
@@ -0,0 +1,64 @@
+import SwiftUI
+import CoreHaptics
+
+struct ContentView: View {
+    let engine = try? CHHapticEngine()
+    let event = CHHapticEvent(eventType: .hapticTransient, parameters: [], relativeTime: 0)
+    @State var isPressed = false
+    @State var isOn = false {
+        didSet {
+            print("here")
+            if self.engine != nil {
+                // If it is - give some haptic feedback
+                let pattern = try? CHHapticPattern(events: [self.event], parameters: [])
+                let player = try? self.engine?.makePlayer(with: pattern!)
+                try? self.engine!.start()
+                try? player?.start(atTime: 0)
+            }
+        }
+    }
+    
+    var body: some View {
+        Color("Background")
+            .edgesIgnoringSafeArea(.all)
+
+        .overlay(
+            VStack {
+                Text("Soft Buttons")
+                    .font(.largeTitle)
+                    .bold()
+                    .shadow(radius: 10)
+
+                Group {
+                    Toggle("", isOn: $isOn)
+                        .toggleStyle(SoftSwitch(width: 70))
+                        .padding(20)
+                    
+                    HStack(spacing: 20) {
+                        SoftToggleButton(text: "1", cornerRadius: 33)
+                            .frame(width: 80, height: 80)
+                        SoftToggleButton(text: "2", cornerRadius: 33)
+                            .frame(width: 80, height: 80)
+                        SoftToggleButton(text: "3", cornerRadius: 33)
+                            .frame(width: 80, height: 80)
+                    }
+                    
+                    Button(action: {
+                        print("gfdg")
+                    }) {
+                        Text("new")
+                            .frame(width: 200, height: 70)
+                    }
+                    .padding(20)
+                    .buttonStyle(SoftButton(width: 70, height: 70, cornerRadius: 50))
+                }
+            }
+        )
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ _SwiftUI-NeumorphicButton/softbutton/SoftSwitch.swift
@@ -0,0 +1,45 @@
+import SwiftUI
+
+struct SoftSwitch: ToggleStyle {
+    var width: CGFloat = 50
+    
+    func makeBody(configuration: Self.Configuration) -> some View {
+        ZStack(alignment: configuration.isOn ? .trailing : .leading) {
+            Button(action: {
+                playHaptic()
+                configuration.isOn.toggle()
+            }, label: {
+                RoundedRectangle(cornerRadius: width/3, style: .circular)
+                    .frame(width: width, height: width/2)
+                    .foregroundColor(Color("Background"))
+                    .overlay(
+                        RoundedRectangle(cornerRadius: width/3)
+                            .stroke(Color("Background"), lineWidth: 4)
+                            .shadow(color: Color("ShadowDark"), radius: 3, x: 3, y: 3)
+                            .clipShape(RoundedRectangle(cornerRadius: width/3))
+                            .shadow(color: Color("ShadowLight"), radius: 3, x: -3, y: -3)
+                            .clipShape(RoundedRectangle(cornerRadius: width/3))
+                    )
+                    .blur(radius: 0.5)
+                    .overlay(
+                        SoftSwitchKnob(isOn: configuration.$isOn, width: width)
+                            .frame(width: width/3, height: width/3)
+                    )
+                    .animation(.spring())
+                })
+        }
+    }
+}
+
+struct SoftSwitchKnob: View {
+    @Binding var isOn: Bool
+    var width: CGFloat = 50
+    
+    var body: some View {
+        Circle()
+            .fill(self.isOn ? .orange : Color("ShadowDark"))
+            .offset(x: self.isOn ? width/5 : -width/5)
+            .shadow(color: Color("ShadowLight"), radius: 3, x: -1, y: -1)
+            .shadow(color: Color("ShadowDark"), radius: 3, x: 1, y: 1)
+    }
+}
