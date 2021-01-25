823c934800749c0069cab8be8c4e2fadcd00efbc
Luan Nguyen <design.luannguyen@gmail.com>
2021-01-25 08:12:40 +0700

    KovSoft-SwiftUI-MVVM-Advance-Map-Kit-Core-Location-Custom-Search-Bar

---
+++ _Colors/Colors.swift
@@ -0,0 +1,19 @@
+import SwiftUI
+
+extension Color {
+    static let tokenBlue = Color("blue")
+    static let tokenBlue2 = Color("blue2")
+    static let tokenDark = Color("dark")
+    static let tokenDark2 = Color("dark2")
+    static let tokenGreen = Color("green")
+    static let tokenLight = Color("light")
+    static let tokenOrange = Color("orange")
+    static let tokenPink = Color("pink")
+    static let tokenPink2 = Color("pink2")
+    static let tokenPurple = Color("purple")
+    static let tokenPurple2 = Color("purple2")
+    static let tokenRed = Color("red")
+    static let tokenYellow = Color("yellow")
+}
+
+// .foregroundColor(.tokenBlue)
