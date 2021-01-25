ef9756c79e0fb3d1cf9312fef5a2be608dc06b2d
Luan Nguyen <design.luannguyen@gmail.com>
2021-01-25 15:20:43 +0700

    Swift-BubbleTransition

---
+++ _Swift-BubbleTransition/Demo/Demo/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by Andrea Mazzini on 13/05/15.
+//  Copyright (c) 2015 Fancy Pixel. All rights reserved.
+//
+
+import UIKit
+import BubbleTransition
+
+class ViewController: UIViewController, UIViewControllerTransitioningDelegate {
+  @IBOutlet weak var transitionButton: UIButton!
+  
+  let transition = BubbleTransition()
+  let interactiveTransition = BubbleInteractiveTransition()
+  
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    if let controller = segue.destination as? ModalViewController {
+      controller.transitioningDelegate = self
+      controller.modalPresentationStyle = .custom
+      controller.modalPresentationCapturesStatusBarAppearance = true
+      controller.interactiveTransition = interactiveTransition
+      interactiveTransition.attach(to: controller)
+    }
+  }
+  
+  // MARK: UIViewControllerTransitioningDelegate
+  
+  func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+    transition.transitionMode = .present
+    transition.startingPoint = transitionButton.center
+    transition.bubbleColor = transitionButton.backgroundColor!
+    return transition
+  }
+  
+  func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+    transition.transitionMode = .dismiss
+    transition.startingPoint = transitionButton.center
+    transition.bubbleColor = transitionButton.backgroundColor!
+    return transition
+  }
+  
+  func interactionControllerForDismissal(using animator: UIViewControllerAnimatedTransitioning) -> UIViewControllerInteractiveTransitioning? {
+    return interactiveTransition
+  }
+  
+}
+
