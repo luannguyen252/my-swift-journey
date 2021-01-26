656fd91a2a6309164fed68a62fb630c5347b5232
Luan Nguyen <design.luannguyen@gmail.com>
2021-01-26 13:38:24 +0700

    SwiftUI-CustomTextField

---
+++ _SwiftUI-CustomTextField/textField/ContentView.swift
@@ -0,0 +1,119 @@
+// DefaultTextFieldStyle
+// PlainTextFieldStyle
+// RoundedBorderTextFieldStyle
+// only for macOS
+// SquareBorderTextFieldStyle
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var text = ""
+    var body: some View {
+        ZStack {
+            Color.white.ignoresSafeArea()
+            VStack(alignment: .leading) {
+                Text("Underlined TextField Style").font(.title2)
+                HStack {
+                    Image(systemName: "magnifyingglass")
+                    TextField("Search", text: $text)
+                }.underlineTextField()
+            }.padding()
+        }
+    }
+}
+
+struct OvalTextFieldStyle: TextFieldStyle {
+    func _body(configuration: TextField<Self._Label>) -> some View {
+        configuration
+            .padding(10)
+            .background(LinearGradient(gradient: Gradient(colors: [Color.orange, Color.orange]), startPoint: .topLeading, endPoint: .bottomTrailing))
+            .cornerRadius(20)
+            .shadow(color: .gray, radius: 10)
+    }
+}
+
+// Custom ViewModifier
+// use .modifier(.customViewModifier(:attributes:))
+struct customViewModifier: ViewModifier {
+    var roundedCornes: CGFloat
+    var startColor: Color
+    var endColor: Color
+    var textColor: Color
+    
+    func body(content: Content) -> some View {
+        content
+            .padding()
+            .background(LinearGradient(gradient: Gradient(colors: [startColor, endColor]), startPoint: .topLeading, endPoint: .bottomTrailing))
+            .cornerRadius(roundedCornes)
+            .padding(3)
+            .foregroundColor(textColor)
+            .overlay(RoundedRectangle(cornerRadius: roundedCornes)
+                        .stroke(LinearGradient(gradient: Gradient(colors: [startColor, endColor]), startPoint: .topLeading, endPoint: .bottomTrailing), lineWidth: 2.5))
+            .font(.custom("Open Sans", size: 18))
+            
+            .shadow(radius: 10)
+    }
+}
+
+// Custom TextField modifier
+// usage TextField.extensionTextFieldView(:attributes:)
+extension TextField {
+    
+    func extensionTextFieldView(roundedCornes: CGFloat, startColor: Color,  endColor: Color) -> some View {
+        self
+            .padding()
+            .background(LinearGradient(gradient: Gradient(colors: [startColor, endColor]), startPoint: .topLeading, endPoint: .bottomTrailing))
+            .cornerRadius(roundedCornes)
+            .shadow(color: .purple, radius: 10)
+    }
+}
+
+extension Color {
+    static let darkPink = Color(red: 208 / 255, green: 45 / 255, blue: 208 / 255)
+}
+
+extension View {
+    func underlineTextField() -> some View {
+        self
+            .padding(.vertical, 10)
+            .overlay(Rectangle().frame(height: 2).padding(.top, 35))
+            .foregroundColor(.darkPink)
+    }
+}
+
+
+
+
+// Neumorphic TextField Style
+
+extension Color {
+    static let lightShadow = Color(red: 255 / 255, green: 255 / 255, blue: 255 / 255)
+    static let darkShadow = Color(red: 163 / 255, green: 177 / 255, blue: 198 / 255)
+    static let background = Color(red: 224 / 255, green: 229 / 255, blue: 236 / 255)
+    static let neumorphictextColor = Color(red: 132 / 255, green: 132 / 255, blue: 132 / 255)
+}
+
+struct NeumorphicStyleTextField: View {
+    var textField: TextField<Text>
+    var imageName: String
+    var body: some View {
+        HStack {
+            Image(systemName: imageName)
+                .foregroundColor(.darkShadow)
+            textField
+            }
+            .padding()
+            .foregroundColor(.neumorphictextColor)
+            .background(Color.background)
+            .cornerRadius(6)
+            .shadow(color: Color.darkShadow, radius: 3, x: 2, y: 2)
+            .shadow(color: Color.lightShadow, radius: 3, x: -2, y: -2)
+        }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
