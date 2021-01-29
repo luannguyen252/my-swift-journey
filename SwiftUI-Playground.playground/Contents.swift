6b681ea4b11839a0545a8e197d68782254a1be8a
Luan Nguyen <design.luannguyen@gmail.com>
2021-01-29 13:17:06 +0700

    SwiftUI Playground

--- SwiftUI-Playground.playground/Contents.swift
+++ SwiftUI-Playground.playground/Contents.swift
@@ -17,3 +17,11 @@ struct ContentView: View {
 
 // Present the view controller in the Live View window
 PlaygroundPage.current.liveView = UIHostingController(rootView: ContentView())
+
+let character: Character = "A"
+character.isLetter
+character.isNumber
+character.isSymbol
+character.isCurrencySymbol
+character.isASCII
+character.isLowercase
