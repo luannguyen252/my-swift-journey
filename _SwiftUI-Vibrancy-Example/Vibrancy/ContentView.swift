59dd00dda51aee1011afa21ec4c9d53ec83a1b3e
Luan Nguyen <design.luannguyen@gmail.com>
2021-01-30 16:49:30 +0700

    SwiftUI-Vibrancy-Example

---
+++ _SwiftUI-Vibrancy-Example/Vibrancy/ContentView.swift
@@ -0,0 +1,64 @@
+import SwiftUIVisualEffects
+import SwiftUI
+
+struct ContentView: View {
+    private var vibrancyLabels: [UIVibrancyEffectStyle] = [ .label, .secondaryLabel, .tertiaryLabel, .quaternaryLabel]
+    private var vibrancyLabelsNames: [String] = [ "Label", "Secondary Label", "Tertiary Label", "Quaternary Label"]
+    
+    private var vibrancyFills: [UIVibrancyEffectStyle] = [ .fill, .secondaryFill, .tertiaryFill]
+    private var vibrancyFillNames: [String] = [ "Fill", "Secondary", "Tertiary"]
+    
+    var body: some View {
+        ZStack {
+            Image("background")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Spacer()
+                
+                ZStack {
+                    Color(.clear).blurEffect()
+                        .blurEffectStyle(.systemThinMaterial)
+                    
+                    TabView {
+                        HStack(spacing: 32) {
+                            ForEach(0 ..< vibrancyFills.count) { i in
+                                VStack(spacing: 16) {
+                                    Image(systemName: "swift")
+                                        .font(.system(size: 60))
+                                        .vibrancyEffect()
+                                        .vibrancyEffectStyle(vibrancyFills[i])
+                                    
+                                    Text(vibrancyFillNames[i])
+                                        .fontWeight(.bold)
+                                        .vibrancyEffect()
+                                        .vibrancyEffectStyle(vibrancyFills[i])
+                                }
+                            }
+                        }
+                        
+                        VStack(alignment: .leading) {
+                            ForEach(0 ..< vibrancyLabels.count) { i in
+                                Text(vibrancyLabelsNames[i])
+                                    .font(Font.title.bold())
+                                    .vibrancyEffect()
+                                    .vibrancyEffectStyle(vibrancyLabels[i])
+                            }
+                        }
+                    }
+                    .tabViewStyle(PageTabViewStyle())
+                }
+                .frame(height: 400)
+            }
+            .edgesIgnoringSafeArea(.all)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
