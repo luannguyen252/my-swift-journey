4da65d49176d747ce06eff097383af6f82044476
Luan Nguyen <design.luannguyen@gmail.com>
2021-01-31 20:54:01 +0700

    Swift-UBottomSheet

---
+++ _Swift-UBottomSheet/Example/UBottomSheet/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  UBottomSheet
+//
+//  Created by OfTheWolf on 09/20/2019.
+//  Copyright (c) 2019 OfTheWolf. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        return true
+    }
+
+
+}
+
