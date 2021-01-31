4e95954ab7e362667fd42240e11d664e0a73943a
Luan Nguyen <design.luannguyen@gmail.com>
2021-01-31 20:02:50 +0700

    SwiftUI-InfiniteList-Scrolling

---
+++ _SwiftUI-InfiniteList-Scrolling/InfiniteListSwiftUI/GithubAPI.swift
@@ -0,0 +1,27 @@
+import Foundation
+import Combine
+
+enum GithubAPI {
+    static let pageSize = 10
+    
+    static func searchRepos(query: String, page: Int) -> AnyPublisher<[Repository], Error> {
+        let url = URL(string: "https://api.github.com/search/repositories?q=\(query)&sort=stars&per_page=\(Self.pageSize)&page=\(page)")!
+        return URLSession.shared
+            .dataTaskPublisher(for: url)
+            .handleEvents(receiveOutput: { print(NSString(data: $0.data, encoding: String.Encoding.utf8.rawValue)!) })
+            .tryMap { try JSONDecoder().decode(GithubSearchResult<Repository>.self, from: $0.data).items }
+            .receive(on: DispatchQueue.main)
+            .eraseToAnyPublisher()
+    }
+}
+
+struct GithubSearchResult<T: Codable>: Codable {
+    let items: [T]
+}
+
+struct Repository: Codable, Identifiable, Equatable {
+    let id: Int
+    let name: String
+    let description: String?
+    let stargazers_count: Int
+}
