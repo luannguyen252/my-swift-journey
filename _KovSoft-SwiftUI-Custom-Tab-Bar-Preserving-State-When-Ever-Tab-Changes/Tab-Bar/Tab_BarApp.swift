d18b300d488873bbc2244084bedacc3f0fefeb10
Luan Nguyen <design.luannguyen@gmail.com>
2021-02-03 17:32:13 +0700

    KovSoft-SwiftUI-Custom-Tab-Bar-Preserving-State-When-Ever-Tab-Changes

---
+++ _KovSoft-SwiftUI-Custom-Tab-Bar-Preserving-State-When-Ever-Tab-Changes/Tab-Bar/Tab_BarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tab_BarApp.swift
+//  Tab-Bar
+//
+//  Created by Balaji on 07/09/20.
+//
+
+import SwiftUI
+
+@main
+struct Tab_BarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
