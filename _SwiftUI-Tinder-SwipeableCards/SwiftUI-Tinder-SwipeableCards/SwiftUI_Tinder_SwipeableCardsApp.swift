3eda0c8393332729fdfb9065f5ccc8a975c7a65d
Luan Nguyen <design.luannguyen@gmail.com>
2021-02-04 17:57:58 +0700

    SwiftUI-Tinder-SwipeableCards

---
+++ _SwiftUI-Tinder-SwipeableCards/SwiftUI-Tinder-SwipeableCards/SwiftUI_Tinder_SwipeableCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Tinder_SwipeableCardsApp.swift
+//  SwiftUI-Tinder-SwipeableCards
+//
+//  Created by Luan Nguyen on 04/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Tinder_SwipeableCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
