d7182ebeedffa793a7d6fa078404a405c2498a84
Luan Nguyen <design.luannguyen@gmail.com>
2021-02-06 08:37:16 +0700

    SwiftUI-Scrollable-Tab-Bar-With-Smooth-Scrolling

---
+++ _KovSoft-SwiftUI-Scrollable-Tab-Bar-With-Smooth-Scrolling-Tab-Effect-View-Builders/Shared/Model/Image.swift
@@ -0,0 +1,7 @@
+import SwiftUI
+
+let image_URLs = [
+    "https://firebasestorage.googleapis.com/v0/b/luan-nguyen-design.appspot.com/o/For-SwiftUI%2Fscreen1%403x.png?alt=media&token=cb96c3dc-6955-4260-9e16-6edc22ee4915",
+    "https://firebasestorage.googleapis.com/v0/b/luan-nguyen-design.appspot.com/o/For-SwiftUI%2Fscreen2%403x.png?alt=media&token=a9deb983-3412-449d-a84a-ce816bc61e62",
+    "https://firebasestorage.googleapis.com/v0/b/luan-nguyen-design.appspot.com/o/For-SwiftUI%2Fscreen3%403x.png?alt=media&token=72192340-2730-47be-a6b0-b96939344a71",
+]
