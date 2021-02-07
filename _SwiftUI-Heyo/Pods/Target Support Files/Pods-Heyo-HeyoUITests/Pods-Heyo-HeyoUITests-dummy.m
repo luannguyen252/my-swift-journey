cbafc472dd9f055addb9cb4b443feb5416491f88
Luan Nguyen <design.luannguyen@gmail.com>
2021-02-07 15:02:22 +0700

    SwiftUI-Heyo

---
+++ _SwiftUI-Heyo/Pods/Target Support Files/Pods-Heyo-HeyoUITests/Pods-Heyo-HeyoUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Heyo_HeyoUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Heyo_HeyoUITests
+@end
---
+++ _SwiftUI-Heyo/Pods/Target Support Files/Pods-HeyoTests/Pods-HeyoTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HeyoTests : NSObject
+@end
+@implementation PodsDummy_Pods_HeyoTests
+@end
