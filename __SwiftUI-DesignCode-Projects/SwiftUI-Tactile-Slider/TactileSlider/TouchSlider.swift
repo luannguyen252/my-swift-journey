caee63c15fd543dd49114afbe022899cb4982ca3
Luan Nguyen <design.luannguyen@gmail.com>
2021-02-07 17:33:42 +0700

    TactileSlider

--- __SwiftUI-DesignCode-Projects/SwiftUI-Tactile-Slider/TactileSlider/TouchSlider.swift
+++ __SwiftUI-DesignCode-Projects/SwiftUI-Tactile-Slider/TactileSlider/TouchSlider.swift
@@ -1,22 +1,10 @@
-//
-//  TouchSlider.swift
-//  TactileSlider
-//
-//  Created by Liu Chuan on 2020/5/13.
-//  Copyright © 2020 Liu Chuan. All rights reserved.
-//
-
 import SwiftUI
 
-/// 触摸滑块视图
 struct TouchSlider: View {
-    
-    /// 百分比
-    @Binding var percentage: Float // 绑定了某些值
+    @Binding var percentage: Float
     
     var body: some View {
         GeometryReader { geometry in
-            // TODO：-可能需要水平和垂直对齐
             ZStack(alignment: .leading) {
                 Rectangle()
                     .foregroundColor(.gray)
@@ -25,10 +13,8 @@ struct TouchSlider: View {
                     .frame(width: geometry.size.width * CGFloat(self.percentage / 100))
             }
             .cornerRadius(12)
-            // 添加手势...
             .gesture(DragGesture(minimumDistance: 0)
                         .onChanged({ (value) in
-                            // TODO：-也许在这里使用其他逻辑
                             self.percentage = min(max(0, Float(value.location.x / geometry.size.width * 100)), 100)
                         }))
         }
