01f2b3eef60e49ca0b023663edc5a31600d12425
Luan Nguyen <design.luannguyen@gmail.com>
2021-02-17 08:25:05 +0700

    SwiftUI - Dismiss The Keyboard

---
+++ __SwiftUI-Dismiss-The-Keyboard/Dismiss-The-Keyboard/Dismiss_The_KeyboardApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@main
+struct Dismiss_The_KeyboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .onAppear(perform: UIApplication.shared.addTapGestureRecognizer)
+        }
+    }
+}
---
+++ __SwiftUI-Dismiss-The-Keyboard/Dismiss-The-Keyboard/Gesture+Extension.swift
@@ -0,0 +1,21 @@
+import SwiftUI
+
+/// How to hide keyboard when using SwiftUI?
+/// https://stackoverflow.com/questions/56491386/how-to-hide-keyboard-when-using-swiftui/63942065#63942065
+
+extension UIApplication {
+    func addTapGestureRecognizer() {
+        guard let window = windows.first else { return }
+        let tapGesture = UITapGestureRecognizer(target: window, action: #selector(UIView.endEditing))
+        tapGesture.requiresExclusiveTouchType = false
+        tapGesture.cancelsTouchesInView = false
+        tapGesture.delegate = self
+        window.addGestureRecognizer(tapGesture)
+    }
+}
+
+extension UIApplication: UIGestureRecognizerDelegate {
+    public func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true // set to `false` if you don't want to detect tap during other gestures
+    }
+}
