3e5e06fdd09e1dd6d54491db9362ee47a91faae2
Luan Nguyen <design.luannguyen@gmail.com>
2021-02-24 07:13:21 +0700

    Dismiss Keyboard

--- __SwiftUI-Dismiss-The-Keyboard/Dismiss-The-Keyboard/ContentView.swift
+++ __SwiftUI-Dismiss-The-Keyboard/Dismiss-The-Keyboard/ContentView.swift
@@ -1,16 +1,29 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var receiptAmount = "0.0"
+    @State private var number   = ""
+    @State private var note     = ""
+    @State private var email    = ""
+    @State private var phone    = ""
     
     var body: some View {
         NavigationView {
             Form {
                 Section {
-                    TextField("Receipt amount", text: $receiptAmount)
+                    TextField("Decimal Pad Keyboard", text: $number)
                         .keyboardType(.decimalPad)
+                    
+                    TextField("Alphabet Keyboard", text: $note)
+                        .keyboardType(.alphabet)
+                    
+                    TextField("Email Address Keyboard", text: $email)
+                        .keyboardType(.emailAddress)
+                    
+                    TextField("Phone Pab Keyboard", text: $phone)
+                        .keyboardType(.phonePad)
                 }
             }
+            .navigationTitle("Dismiss Keyboard")
         }
     }
 }
@@ -19,6 +32,7 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
     }
 }
 #endif
