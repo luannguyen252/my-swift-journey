6c9e5bdb0d74f4fba024930a8b6e617439d7ff90
Luan Nguyen <design.luannguyen@gmail.com>
2021-03-12 09:38:53 +0700

    Custom Time Picker Circular Slider Android Style Time Picker

---
+++ ___KovSoft-SwiftUI-Tutorials/Custom-Time-Picker-Circular-Slider-Android-Style-Time-Picker/Shared/Date_Picker_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Date_Picker_ChallengeApp.swift
+//  Shared
+//
+//  Created by Balaji on 11/03/21.
+//
+
+import SwiftUI
+
+@main
+struct Date_Picker_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
